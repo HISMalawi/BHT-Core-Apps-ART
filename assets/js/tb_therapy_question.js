d6135ac8f2a658f3f123285e09210fccd0793fcb
mwatha <mwathabwanali@gmail.com>
2020-08-24 11:37:26 +0200

    adding 3HP prescription  - 95% done: working on MoH feedback

--- assets/js/tb_therapy_question.js
+++ assets/js/tb_therapy_question.js
@@ -1,7 +1,7 @@
 
 
 function checkIfEverCompletedTBtherapyAsked(){
-  return false
+  return ever_completed_tb_therapy;
 }
 
 
@@ -16,12 +16,57 @@ function addTBtherapyYesNo(){
 }
 
 
-function everCompleteTBtherapty(){
-  var three_hp = (yesNo_Hash["Previous TB treatment history"]["3HP (RFP + INH)?"] == "Yes");
-  var ipt =  (yesNo_Hash["Previous TB treatment history"]["IPT?"] == "Yes");
+function everCompleteTBtherapy(){
+  try {
+    var three_hp = (yesNo_Hash["Previous TB treatment history"]["3HP (RFP + INH)?"] == "Yes");
+    var ipt =  (yesNo_Hash["Previous TB treatment history"]["IPT?"] == "Yes");
+  }catch(e){
+    show_completed_tb_therapy_location = true;
+    return ever_completed_tb_therapy;
+  }
 
-  if(ipt == true || three_hp == true)
+
+  if(ipt == true || three_hp == true){
+    show_completed_tb_therapy_location = true;
     return true;
+  }
 
+  show_completed_tb_therapy_location = false;
   return false;
 }
+
+function showTBtherapyLocation(){
+  everCompleteTBtherapy();
+  return show_completed_tb_therapy_location;
+}
+
+var ever_completed_tb_therapy = false;
+var show_completed_tb_therapy_location = false;
+
+
+function getTBtheraptObs() {
+  var previous_tb_treatment_history = 1588;
+  var url = apiProtocol + "://" + apiURL + ":" + apiPort + "/api/v1/observations?person_id=";
+  url += sessionStorage.patientID + "&concept_id=" + previous_tb_treatment_history + "&page_size=10";
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && (this.status == 201 || this.status == 200)) {
+          var tb_history_obs = JSON.parse(this.responseText);
+          for (var i = 0; i < tb_history_obs.length; i++) {
+            var ob_children = tb_history_obs[i].children;
+            for (var c = 0; c < ob_children.length; c++) {
+             if(ob_children[c].value_coded == 1065)
+               ever_completed_tb_therapy = true;
+
+          }
+        }
+      }
+  };
+
+  xhttp.open("GET", url, true);
+  xhttp.setRequestHeader('Authorization', sessionStorage.getItem("authorization"));
+  xhttp.setRequestHeader('Content-type', "application/json");
+  xhttp.send();
+}
+
+getTBtheraptObs();
